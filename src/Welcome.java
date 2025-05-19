57bfbf046e4d1153b95afb4ff3baa46a8a42dfdd
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-19 11:29:32 +0900

    Add pseudocode for selectable menu list display method

--- src/Welcome.java
+++ src/Welcome.java
@@ -21,12 +21,16 @@ public class Welcome {
             System.out.println("**************************************************");
             System.out.println("\t" + greeting);
             System.out.println("\t" + tagline);
+
+            /*
             System.out.println("**************************************************");
             System.out.println("1. 고객 정보 확인하기 \t\t5. 바구니에 항목 추가하기");
             System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
             System.out.println("3. 장바구니 비우기 \t\t7. 장바구니의 항목 삭제하기");
             System.out.println("4. 영수증 표시하기 \t\t8. 종료");
             System.out.println("**************************************************");
+             */
+            // 메뉴 목록 출력 메서드 호출
 
             System.out.print("메뉴 번호를 선택해주세요.");
             int n = input.nextInt();
@@ -64,5 +68,6 @@ public class Welcome {
                 } // switch문
             } //if~else문
         } //while문
-    }
+    }//main 메서드
+    //메뉴 목록 출력 메서드 선언 및 메뉴 목록 작성
 }
