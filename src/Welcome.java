b82b2003b9e947852ff544086cbe8bae60ff9c2e
dev-burnern <jordash1044@gmail.com>
2025-05-19 10:30:42 +0900

    Write pseudocode to select a main menu option

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,6 +1,27 @@
+// 스캐너 클래스 패키지 임포트
+
 public class Welcome {
     public static void main(String[] args) {
+        // 스캐너 클래스의 객체 생성
+
+        // 이름을 문자열로 입력받기
+
+        // 연락처를 정수로 입력받음 ( - 사용 X )
+
         System.out.println("Welcome to Shopping Mall");
         System.out.println("Welcome to BookMarket");
+
+        // 인사말 변수 선언 및 초기화
+        // 태그 라인 변수 선언 및 초기화
+
+        // * 20~30 개 출력 ( 구분선 )
+        // 인사말 출력
+        // 태그 라인 출력
+        // * 20~30 개 출력 ( 구분선 )
+        // 메뉴 출력
+        // * 20~30 개 출력 ( 구분선 )
+        // 메뉴 번호 선택 출력
+        // 메뉴 번호 입력
+        // 입력한 번호 출력
     }
 }
