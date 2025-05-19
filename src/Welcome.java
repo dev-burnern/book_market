8818a9f9a659360bee748abdf0505c7b8b084150
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-19 11:14:31 +0900

    Menu information output

--- src/Welcome.java
+++ src/Welcome.java
@@ -28,6 +28,38 @@ public class Welcome {
 
         System.out.print("메뉴 번호를 선택해주세요.");
         int n = input.nextInt();
-        System.out.println(n + "번을 선택했습니다.");
+        System.out.println(n + "번을 선택했습니다."); // 주석 처리
+
+        if (n < 1 || n > 8) {
+            System.out.println("1부터 8까지의 숫자를 입력하세요");
+        }
+        else {
+            switch (n) {
+                case 1:
+                    System.out.println("1. 현재 고객 정보 : ");
+                    break;
+                case 2:
+                    System.out.println("2. 장바구니 상품 목록 보기 :");
+                    break;
+                case 3:
+                    System.out.println("3. 장바구니 비우기");
+                    break;
+                case 4:
+                    System.out.println("4. 장바구니에 항목 추가하기 : ");
+                    break;
+                case 5:
+                    System.out.println("5. 장바구니의 항목 수량 줄이기");
+                    break;
+                case 6:
+                    System.out.println("6. 장바구니의 항목 삭제하기");
+                    break;
+                case 7:
+                    System.out.println("7. 영수증 표시하기");
+                    break;
+                case 8:
+                    System.out.println("8. 종료");
+                    break;
+            } // switch문
+        } //if~else문
     }
 }
