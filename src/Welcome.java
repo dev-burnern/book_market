6fcfef87098b1a4260fa5085dd30216000a1f4f0
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-19 10:49:22 +0900

    Implement the main menu selection feature

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,27 +1,33 @@
-// 스캐너 클래스 패키지 임포트
+import java.util.Scanner;
 
 public class Welcome {
     public static void main(String[] args) {
-        // 스캐너 클래스의 객체 생성
+        Scanner input = new Scanner(System.in);
 
-        // 이름을 문자열로 입력받기
+        System.out.print("당신의 이름을 입력하세요");
+        String userName = input.next();
 
-        // 연락처를 정수로 입력받음 ( - 사용 X )
+        System.out.print("연락처를 입력하세요 :");
+        int userMobile = input.nextInt();
 
-        System.out.println("Welcome to Shopping Mall");
-        System.out.println("Welcome to BookMarket");
+//        System.out.println("Welcome to Shopping Mall");
+//        System.out.println("Welcome to BookMarket");
 
-        // 인사말 변수 선언 및 초기화
-        // 태그 라인 변수 선언 및 초기화
+        String greeting = "Welcome to Shopping Mall";
+        String tagline = "Welcome to Book Market";
 
-        // * 20~30 개 출력 ( 구분선 )
-        // 인사말 출력
-        // 태그 라인 출력
-        // * 20~30 개 출력 ( 구분선 )
-        // 메뉴 출력
-        // * 20~30 개 출력 ( 구분선 )
-        // 메뉴 번호 선택 출력
-        // 메뉴 번호 입력
-        // 입력한 번호 출력
+        System.out.println("**************************************************");
+        System.out.println("\t" + greeting);
+        System.out.println("\t" + tagline);
+        System.out.println("**************************************************");
+        System.out.println("1. 고객 정보 확인하기 \t\t5. 바구니에 항목 추가하기");
+        System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
+        System.out.println("3. 장바구니 비우기 \t\t7. 장바구니의 항목 삭제하기");
+        System.out.println("4. 영수증 표시하기 \t\t8. 종료");
+        System.out.println("**************************************************");
+
+        System.out.print("메뉴 번호를 선택해주세요.");
+        int n = input.nextInt();
+        System.out.println(n + "번을 선택했습니다.");
     }
 }
