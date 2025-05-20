fe0921cdb52def674ad54dc4e7e4fdc9dd3a2ed1
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-20 09:45:53 +0900

    feat: Menuadminlogin function implementation

--- src/Welcome.java
+++ src/Welcome.java
@@ -207,4 +207,21 @@ public class Welcome {
         }
         return flag;
     }
+    public static void menuAdminLogin() {
+        System.out.println("관리자 정보를 입력하세요");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("아이디 : ");
+        String adminId = input.next();
+
+        System.out.print("비밀번호 : ");
+        String adminPW = input.next();
+
+        Admin admin = new Admin(mUser.getName(), mUser.getPhone());
+        if (adminId.equals(admin.getId()) && adminPW.equals(admin.getPassward())){
+            System.out.println("이름 " + admin.getName() + "  연락처 " + admin.getPhone());
+            System.out.println("아이디 " + admin.getId() + "  비밀번호 " + admin.getPassward());
+        }else
+            System.out.println("관리자 정보가 일치하지 않습니다.");
+    }
 }
