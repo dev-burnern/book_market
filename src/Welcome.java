db64b50d6773cce2da8d852bed9d27b3a5247e1e
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-20 09:34:08 +0900

    refactor : Menuintroduction Content Modification and Administrator Login Output

--- src/Welcome.java
+++ src/Welcome.java
@@ -90,10 +90,11 @@ public class Welcome {
     }//main 메서드
     public static void menuIntroduction() {
         System.out.println("**************************************************");
-        System.out.println("1. 고객 정보 확인하기 \t\t5. 바구니에 항목 추가하기");
-        System.out.println("2. 장바구니 상품 목록 보기 \t6. 장바구니의 항목 수량 줄이기");
-        System.out.println("3. 장바구니 비우기 \t\t7. 장바구니의 항목 삭제하기");
-        System.out.println("4. 영수증 표시하기 \t\t8. 종료");
+        System.out.println("1. 고객 정보 확인하기 \t\t\t2. 장바구니 상품 목록 보기");
+        System.out.println("3. 장바구니 비우기 \t\t\t4. 장바구니에 항목 추가하기");
+        System.out.println("5. 장바구니의 항목 수량 줄이기 \t6. 장바구니의 항목 삭제하기");
+        System.out.println("7. 영수증 표시하기 \t\t\t8. 종료");
+        System.out.println("9. 관리자 로그인");
         System.out.println("**************************************************");
     }
     public static void menuGuestInfo(String name, int mobile) {
