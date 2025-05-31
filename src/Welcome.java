d7a718bd913e812d8f81258c2715029c1395cd34
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-31 23:08:46 +0900

    feat : 도서 정보 저장

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class Welcome {
+    static final int NUM_BOOK = 3; // 도서의 개수에 대한 상수
+    static final int NUM_ITEM = 7; // 각 도서에 대한 항목의 개수에 대한 상수
+
     public static void main(String[] args) {
+        String[][] mBook = new String[NUM_BOOK][NUM_ITEM]; // 도서 정보 2차원 배열
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("당신의 이름을 입력하세요 : ");
@@ -119,5 +124,32 @@ public class Welcome {
 
     public static void menuExit() {
         System.out.println("8. 종료");
+    }
+
+    public static void BookList(String[][] book) {
+
+        book[0][0] = "ISBN1234";
+        book[0][1] = "쉽게 배우는 JSP 웹 프로그래밍";
+        book[0][2] = "27000";
+        book[0][3] = "송미영";
+        book[0][4] = "단계별로 쇼핑몰을 구현하며 배우는 JSP 웹 프로그래밍";
+        book[0][5] = "IT전문서";
+        book[0][6] = "2018/10/08";
+
+        book[1][0] = "ISBN1235";
+        book[1][1] = "안드로이드 프로그래밍";
+        book[1][2] = "33000";
+        book[1][3] = "우재남";
+        book[1][4] = "실습 단계별 명쾌한 멘토링!";
+        book[1][5] = "IT전문서";
+        book[1][6] = "2022/01/22";
+
+        book[2][0] = "ISBN1236";
+        book[2][1] = "스크래치";
+        book[2][2] = "22000";
+        book[2][3] = "고광일";
+        book[2][4] = "컴퓨팅 사고력을 키우는 블록 코딩";
+        book[2][5] = "컴퓨터입문";
+        book[2][6] = "2019/06/10";
     }
 }
