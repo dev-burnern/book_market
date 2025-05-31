589975a048970cd48b38329c6584201ad003468c
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-31 20:39:41 +0900

    feat : Scanner 메서드를 이용하여 이름과 연락처 입력 받기

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,6 +4,12 @@ public class Welcome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); // input 객체 생성
 
+        System.out.print("당신의 이름을 입력하세요 : ");
+        String userName = input.next();
+
+        System.out.print("연락처를 입력하세요 : ");
+        int userMobile = input.nextInt();
+
 //        System.out.println("Welcome to Shopping Mall");
 //        System.out.println("Welocme to Book Market");
 
