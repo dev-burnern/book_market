3c95f109aed58582ac2008959495a4b14b6d69a6
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-31 19:56:18 +0900

    feat : 메인 메뉴 항목 출력

--- src/Welcome.java
+++ src/Welcome.java
@@ -7,9 +7,14 @@ public class Welcome {
         String greeting = "Welcome to Shopping Mall";
         String tagline = "Welcome to Book Market";
 
-        System.out.println("****************************************");
-        System.out.println("\t" + greeting);
-        System.out.println("\t" + tagline);
-        System.out.println("****************************************");
+        System.out.println("**********************************************************");
+        System.out.println("\t\t\t\t" + greeting);
+        System.out.println("\t\t\t\t" + tagline);
+        System.out.println("***********************************************************");
+
+        System.out.println(" 1. 고객 정보 확인하기 \t\t4. 바구니에 항목 추가하기");
+        System.out.println(" 2. 장바구니 상품 목록 보기 \t5. 장바구니의 항목 수량 줄이기");
+        System.out.println(" 3. 장바구니 비우기 \t\t\t6. 장바구니의 항목 삭제하기");
+        System.out.println(" 7. 영수증 표시하기 \t\t\t8. 종료");
     }
 }
