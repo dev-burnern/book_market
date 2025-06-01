84a96e67188b05e0b54e4859756a1f6ea524232e
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-06-01 20:19:05 +0900

    feat : 사용자 클래스 생성

---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User extends Person {
+
+    public User(String name, String phone) {
+        super(name, phone);
+    }
+
+    public User(String username, String phone, String address) {
+        super(username, phone, address);
+    }
+}
