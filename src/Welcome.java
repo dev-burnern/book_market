c52a0527438ed29d4783f9fdad1d2ffdb918b072
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-06-01 20:36:33 +0900

    feat : 관리자 로그인 정보 확인 메서드 생성

--- src/Welcome.java
+++ src/Welcome.java
@@ -232,4 +232,21 @@ public class Welcome {
         }
         return flag;
     }
+    public static void menuAdminLogin() {
+        System.out.println("관리자 정보를 입력하세요.");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("아이디 : ");
+        String adminId = input.next();
+
+        System.out.print("비밀번호 : ");
+        String adminPw = input.next();
+
+        Admin admin = new Admin(mUser.getName(), mUser.getPhone());
+        if (adminId.equals(admin.getId()) && adminPw.equals(admin.getPassword())) {
+            System.out.println("이름 : " + admin.getName() + ", 연락처: " + admin.getPhone());
+            System.out.println("아이디 : " + admin.getId() + ", 비밀번호: " + admin.getPassword());
+        }else
+            System.out.println("관리자 정보가 일치하지 않습니다. 다시 시도해주세요.");
+    }
 }
